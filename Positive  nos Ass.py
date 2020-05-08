cf09400e76e91da8f091d06211a1fe487c38e862
Midhu-Susan <64888715+Midhu-Susan@users.noreply.github.com>
2020-05-08 12:15:47 +0530

    Add files via upload

---
+++ Positive  nos Ass.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+input =[12,-7,5,64,-14]
+print ("Input:List1=",input)
+enum=[]
+for i in input :
+    if i > 0:
+        enum.append(i)
+print("output",enum)
+# second set
+#break
+input = [12,14,-95,3]
+print ("Input:List2=",input)
+posnum=[]
+for i in input :
+    posnum.append(i)
+
+print ("output",posnum)
+
+
+            
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
